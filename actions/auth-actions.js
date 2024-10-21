d6882de230f39ea12c717af962223fca66532aa8
Dana Sharon <whosedreamisthis@gmail.com>
2024-10-21 12:47:19 -0700

    showing errors if signup form entries are invalid

---
+++ actions/auth-actions.js
@@ -0,0 +1,21 @@
+'use server';
+
+export async function signup(prevState, formData) {
+	const email = formData.get('email');
+	const password = formData.get('password');
+
+	let errors = {};
+	if (!email.includes('@')) {
+		errors.email = 'Please enter a valid email address.';
+	}
+
+	if (password.trim().length < 8) {
+		errors.password = 'password must be at least 8 characters long.';
+	}
+
+	if (Object.keys(errors).length > 0) {
+		return {
+			errors,
+		};
+	}
+}
--- components/auth-form.js
+++ components/auth-form.js
@@ -1,27 +1,35 @@
+'use client';
 import Link from 'next/link';
-
+import { useFormState } from 'react-dom';
+import { signup } from '@/actions/auth-actions';
 export default function AuthForm() {
-  return (
-    <form id="auth-form">
-      <div>
-        <img src="/images/auth-icon.jpg" alt="A lock icon" />
-      </div>
-      <p>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-      </p>
-      <p>
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-      </p>
-      <p>
-        <button type="submit">
-          Create Account
-        </button>
-      </p>
-      <p>
-        <Link href="/">Login with existing account.</Link>
-      </p>
-    </form>
-  );
+	const [formState, formAction] = useFormState(signup, {});
+	return (
+		<form id="auth-form" action={formAction}>
+			<div>
+				<img src="/images/auth-icon.jpg" alt="A lock icon" />
+			</div>
+			<p>
+				<label htmlFor="email">Email</label>
+				<input type="email" name="email" id="email" />
+			</p>
+			<p>
+				<label htmlFor="password">Password</label>
+				<input type="password" name="password" id="password" />
+			</p>
+			{formState.errors && (
+				<ul id="form-errors">
+					{Object.keys(formState.errors).map((e) => {
+						return <li key={e}>{formState.errors[e]}</li>;
+					})}
+				</ul>
+			)}
+			<p>
+				<button type="submit">Create Account</button>
+			</p>
+			<p>
+				<Link href="/">Login with existing account.</Link>
+			</p>
+		</form>
+	);
 }
